90988941b499095ab03675ded579f888febdae80
Skiiicodle <121371082+Skiiidoodle@users.noreply.github.com>
2024-02-16 22:38:18 +0700

    Update Exercise8.java

--- assignment3/Exercise8.java
+++ assignment3/Exercise8.java
@@ -1,5 +1,6 @@
 public class Exercise8 {
     public static void main(String[] args) {
+
         // make a boolean with size of 51 but use 1 to 50
         boolean[] lockers = new boolean[51];
         // count how many open lockers are there
@@ -7,11 +8,18 @@ public class Exercise8 {
 
         // lockers 1 is open and the rest is close 
         // according to the exercise
-        lockers[1] = true;
-        
-        // start from player 3 because that's when
-        // we start to count L3 L6, L4 L8
-        for(int p = 3; p < lockers.length; p++){
+        for(int p = 1; p < lockers.length; p++){
+
+            // we close/open every locker with 1 increment
+            // if the player is 1 or 2
+            if(p == 1 || p ==2){
+                for(int i = p; i < lockers.length; i++){
+                    // swap open to close and close to open
+                    lockers[i] = !lockers[i];
+                }
+                continue;
+            }
+
             // we start from p and increment according to p
             // eg start from 3 and increment to 6, 9, 12...
             for(int i = p; i < lockers.length; i+=p){
@@ -28,5 +36,6 @@ public class Exercise8 {
             }
         }
         System.out.print("\nTotal number of open lockers: " + count);
+      
     }
 }
